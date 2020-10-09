61ec4474bc2a174e472106b02fb306a2a30f4a7d
livoras <livoras@163.com>
2020-10-09 11:05:46 +0800

    webpack

--- webpack.config.js
+++ webpack.config.js
@@ -28,8 +28,7 @@ module.exports = {
         ],
     },
     output: {
-        // filename: 'main.js',
-        path: path.resolve(__dirname, 'dist'),
+        path: path.resolve(__dirname, 'dist')
     },
 };
 
